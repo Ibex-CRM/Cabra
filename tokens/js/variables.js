0dd8a10859fa6d94c35e861d8431eaa6eb59d313
Carlos-Diaz-07 <Carlos-Diaz-07@users.noreply.github.com>
2022-11-07 14:31:12 +0000

    Update tokens

--- tokens/js/variables.js
+++ tokens/js/variables.js
@@ -6,6 +6,7 @@ export const TokenSetOrder0 = "global";
 export const TokenSetOrder1 = "new";
 export const Bvn = "#988b8b";
 export const BlueNew = "#18395e";
+export const C = "#a51515";
 export const Medium = 5;
 export const PaddingM = 25;
 export const Red = "#bf0505";
